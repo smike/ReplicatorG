f65d829a44788f5b37d5092f64d8ad2589648511
smike <smike@alum.mit.edu>
2012-01-27 01:36:56 -0500

    Making the direct driver send one command at a time so that pause will be more effective. Currently it pauses only when all commands are processed.

--- src/replicatorg/machine/builder/Direct.java
+++ src/replicatorg/machine/builder/Direct.java
@@ -25,85 +25,85 @@ import replicatorg.model.GCodeSource;
  *
  */
 public class Direct implements MachineBuilder{
-	
+
 	public enum State {
 		RUNNING_GCODE,
 		WAITING_FOR_MACHINE_FINISH,
 		FINISHED
 	}
-	
+
 	GCodeSource source;
 	Iterator<String> i;
-	
+
 	int linesProcessed;
 	int linesTotal;
-	
+
 	Driver driver;
 	GCodeParser parser;
 	Queue<DriverCommand> driverQueue;
 	boolean building;		// True if we are running to the machine
-	
+
 	SimulationDriver simulator;
 	GCodeParser simulationParser;
 	Queue<DriverCommand> simulatorQueue;
 	boolean simulating;		// True if we are running to the simulator
-	
+
 	boolean retry = false;
-	
+
 	State state;
-	
+
 	public Direct(Driver driver, SimulationDriver simulator, GCodeSource source) {
 		this.driver = driver;
 		this.source = source;
-	
+
 		linesProcessed = 0;
 		linesTotal = source.getLineCount();
-		
+
 		// Initialize our gcode provider
 		i = source.iterator();
-		
+
 		if (driver == null) {
 			building = false;
 		} else {
 			building = true;
 			// Set up a parser to talk to the driver
 			parser = new GCodeParser();
-			
+
 			// Queue of commands that we get from the parser, and run on the driver.
 			driverQueue = new LinkedList< DriverCommand >();
-			
+
 			parser.init((DriverQueryInterface) driver);
 		}
-		
+
 		if (simulator == null) {
 			simulating = false;
 		} else {
 			simulating = true;
-			
+
 			this.simulator = simulator;
 			// And the one for the simulator
 			simulationParser = new GCodeParser();
-			
+
 			// Queue of commands that we get from the parser, and run on the driver.
 			simulatorQueue = new LinkedList< DriverCommand >();
-			
+
 			simulationParser.init((DriverQueryInterface) simulator);
 		}
-		
+
 		simulating = false;
-	
+
 		state = State.RUNNING_GCODE;
 	}
-	
-	
+
+
 	@Override
 	public boolean finished() {
 		return (state == State.FINISHED);
 	}
-	
+
 	// Run the next command on the driver
 	@Override
-	public void runNext() { 
+	public void runNext() {
 		if (!i.hasNext()) {
 			// TODO: This is clunky.
 			if (driver.isFinished()) {
@@ -113,30 +113,36 @@ public class Direct implements MachineBuilder{
 			}
 			return;
 		}
-		
+
+		if (!driver.isFinished()) {
+		  // Still running the last line. Wait.
+      return;
+    }
+
 		// Check for any driver errors
 		// TODO: Do we need to check if we are building?
 		if (building && driver.hasError()) {
 			Base.logger.severe("Driver reported an error, aborting build");
 			return;
 		}
-		
+
 		// Read and process next line
 		if (retry == false) {
 			String line = i.next();
+			Base.logger.info("Processing line: " + line);
 			linesProcessed++;
 
 			// Parse a line for the actual machine
 			if (building) {
 				parser.parse(line, driverQueue);
 			}
-			
-			// If we're simulating, parse a line to feed to the simulator 
+
+			// If we're simulating, parse a line to feed to the simulator
 			if (simulating) {
 				simulationParser.parse(line, simulatorQueue);
 			}
 		}
-		
+
 		// Simulate the command. Just run everything against the simulator, and ignore errors.
 		if (retry == false && simulating) {
 			for (DriverCommand command : simulatorQueue) {
@@ -150,7 +156,7 @@ public class Direct implements MachineBuilder{
 			}
 			simulatorQueue.clear();
 		}
-		
+
 		try {
 			if (building) {
 				// Run the command on the machine.
@@ -159,7 +165,7 @@ public class Direct implements MachineBuilder{
 					driverQueue.remove();
 				}
 			}
-			
+
 			retry = false;
 		} catch (RetryException r) {
 			// Indicate that we should retry the current line, rather
@@ -168,10 +174,10 @@ public class Direct implements MachineBuilder{
 			retry = true;
 		} catch (StopException e) {
 			// TODO: Just returning here seems dangerous, better to notify the state machine.
-			
+
 			switch (e.getType()) {
 			case UNCONDITIONAL_HALT:
-				JOptionPane.showMessageDialog(null, e.getMessage(), 
+				JOptionPane.showMessageDialog(null, e.getMessage(),
 						"Unconditional halt: build ended", JOptionPane.INFORMATION_MESSAGE);
 				state = State.FINISHED;
 				break;
@@ -183,7 +189,7 @@ public class Direct implements MachineBuilder{
 			case OPTIONAL_HALT:
 				int result = JOptionPane.showConfirmDialog(null, e.getMessage(),
 						"Optional halt: Continue build?", JOptionPane.YES_NO_OPTION);
-				
+
 				if (result == JOptionPane.YES_OPTION) {
 					driverQueue.remove();
 				} else {
@@ -198,7 +204,7 @@ public class Direct implements MachineBuilder{
 			}
 		}
 	}
-	
+
 	public int getLinesTotal() {
 		return linesTotal;
 	}
